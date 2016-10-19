fface8a7bf1eb412de80275db6998b281ab08c58
Andrea Pascal <andrea@anodium.net>
2016-10-19 15:44:23 -0400

    Add logo tweening

--- source/cinche/TitleState.hx
+++ source/cinche/TitleState.hx
@@ -3,18 +3,26 @@ package cinche;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.tweens.FlxTween;
 import openfl.Assets;
 
 class TitleState extends FlxState
 {
 	private var _logo:FlxSprite;
+	private static inline var _LOGO_X = 15;
+	private static inline var _LOGO_Y = 8;
+
+	private var _tween:FlxTween;
 
 	override public function create():Void
 	{
 		super.create();
 
-		_logo = new FlxSprite(15, 8, Assets.getBitmapData("assets/images/logo.png"));
+		_logo = new FlxSprite(-_LOGO_X*2, _LOGO_Y, Assets.getBitmapData("assets/images/logo.png"));
+		_logo.pixelPerfectRender = true;
+		_tween = FlxTween.linearMotion(_logo, -_LOGO_X*2, _LOGO_Y, _LOGO_X, _LOGO_Y, 1.5, true);
 		add(_logo);
+		_tween.start();
 	}
 
 	override public function update(elapsed:Float):Void
