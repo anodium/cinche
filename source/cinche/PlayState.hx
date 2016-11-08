883e90c987e4595a38770bf0b8a42401a5d0476c
Andrea Pascal <andrea@anodium.net>
2016-11-08 01:25:18 -0400

    Add boilerplate for PlayState MVP

--- source/cinche/PlayState.hx
+++ source/cinche/PlayState.hx
@@ -5,13 +5,33 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
+import flixel.util.FlxColor;
 import flixel.math.FlxMath;
 
 class PlayState extends FlxState
 {
+	private var ditB:FlxSprite = new FlxSprite();
+	private static inline var DIT_B_X = 4;
+	private static inline var DIT_B_Y = 32;
+
+	private var ditO:FlxSprite = new FlxSprite();
+	private static inline var DIT_O_X = 60;
+	private static inline var DIT_O_Y = 32;
+
 	override public function create():Void
 	{
 		super.create();
+
+		ditB.makeGraphic(1, 1, FlxColor.BLUE);
+		ditB.x = DIT_B_X;
+		ditB.y = DIT_B_Y;
+
+		ditO.makeGraphic(1, 1, FlxColor.ORANGE);
+		ditO.x = DIT_O_X;
+		ditO.y = DIT_O_Y;
+
+		add(ditB);
+		add(ditO);
 	}
 
 	override public function update(elapsed:Float):Void
