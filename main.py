a250cc9027b2a8adc42e237394258fe589fe9eb2
Andrea Pascal <andrea@anodium.net>
2018-01-09 09:57:34 -0400

    Migrate from Kivy to Cocos2D

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import kivy
-kivy.require('1.8.0')
 
-from cinche.cincheapp import CincheApp
+def _main():
+    pass
+
 
 if __name__ == "__main__":
-    CincheApp().run()
+    _main()
