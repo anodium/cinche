0374bf2049d34a2f4d504ed7ef5f713d33cd7ec7
Andrea Pascal <andrea@anodium.net>
2018-01-11 10:52:32 -0400

    Disable anti-aliasing

--- main.py
+++ main.py
@@ -4,15 +4,19 @@
 from cinche.title import TitleLayer
 from cocos.director import director
 from cocos.scene import Scene
+from pyglet.gl import glEnable, glTexParameteri
+from pyglet.gl import GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST
+
 
 if __name__ == "__main__":
     director.init(caption='Cinche', height=64, width=64, autoscale=True)
     # TODO: This expects a pyglet.image.AbstractImage
-    director.window.set_icon('assets/images/icon.png')
-    # TODO: Blurry as hell, but works
-    director.window.set_size(512, 512)
+    # director.window.set_icon('assets/images/icon.png')
 
+    director.window.set_size(512, 512)
     title_layer = TitleLayer()
+    glEnable(GL_TEXTURE_2D)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
 
     main_scene = Scene(title_layer)
 
