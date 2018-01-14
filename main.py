a3d934dd90420b2f291bbdf1fa73365db0311762
Andrea Pascal <andrea@anodium.net>
2018-01-14 09:56:44 -0400

    Add window icon

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from cocos.director import director
 from cocos.scene import Scene
 from pyglet.gl import glEnable, glTexParameteri
 from pyglet.gl import GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST
+from pyglet.image import load
 
 
 if __name__ == "__main__":
     director.init(caption='Cinche', height=64, width=64, autoscale=True)
-    # TODO: This expects a pyglet.image.AbstractImage
-    # director.window.set_icon('assets/images/icon.png')
+    director.window.set_icon(load('assets/images/icon.png'))
     director.window.set_size(512, 512)
 
     title_scene = Scene(TitleLayer())
